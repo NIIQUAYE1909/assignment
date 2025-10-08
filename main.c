b45899dc827664880454abd04b49e4e9bdb647bd
AddyMaine <addymaine22@gmail.com>
2025-10-08 14:38:04 +0000

    initial commit

--- main.c
+++ main.c
@@ -2,20 +2,40 @@
 #include <string.h>
 
 int main(){
-
-     // MAD LIBS GAME
-
      char noun[50] = "";
      char verb[50] = "";
      char adjective1[50] = "";
      char adjective2[50] = "";
      char adjective3[50] = "";
 
-     printf("Enter an adjective: ");
-     scanf("%s", &adjective1);
+     printf("Enter an adjective (description): ");
+     fgets(adjective1, sizeof(adjective1), stdin);
+     adjective1[strlen(adjective1) - 1] = '\0'; // Remove newline character
 
-     printf()
+     printf("Enter a noun (person, place, or thing): ");
+     fgets(noun, sizeof(noun), stdin);
+     noun[strlen(noun) - 1] = '\0'; // Remove newline character
 
+     printf("Enter an adjective (description): ");
+     fgets(adjective2, sizeof(adjective2), stdin);
+     adjective2[strlen(adjective2) - 1] = '\0'; // Remove newline character
 
+     printf("Enter a verb (ending with-ing): ");
+     fgets(verb, sizeof(verb), stdin);
+     verb[strlen(verb) - 1] = '\0'; // Remove newline character
+
+     printf("Enter an adjective (description): ");
+     fgets(adjective3, sizeof(adjective3), stdin);
+     adjective3[strlen(adjective3) - 1] = '\0'; // Remove newline character
+
+     printf("%s\n", noun);
+     printf("%s\n", verb);
+     printf("%s\n", adjective1);
+     printf("%s\n", adjective2);
+     printf("%s\n", adjective3);
+     printf("Today I went to a %s zoo.\n", adjective1);
+     printf("In an exhibit i saw a %s.\n", noun);
+     printf("%s was %s and %s!. \n", noun, adjective2, verb);
+     printf("I was %s.\n", adjective3);
      return 0;
 }
