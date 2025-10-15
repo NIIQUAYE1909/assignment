015e6ef914dc826dafe08fb27a719b4d097815dd
AddyMaine <addymaine22@gmail.com>
2025-10-15 14:47:02 +0000

    SIMULATED BANKING SYSTEM IN C

--- main.c
+++ main.c
@@ -1,14 +1,82 @@
 #include <stdio.h>
-#include <windows.h>
 
+void checkBalance(float balance);
+float deposit();
+float withDraw(float balance);
 
 int main(){
 
-     for(int i = 10; i >= 0; i--){
-          Sleep(1000);
-          printf("%d\n", i);
+     // BANKING PROGRAM
+
+     int choice = 0;
+     float balance = 0.0f;
+
+     printf("*** WLCOME TO THE BANK ***");
+
+     do{
+          printf("\nSelect an option: ");
+          printf("\n1. Check balance");
+          printf("\n2. Deposit Money");
+          printf("\n3. Withdraw Money");
+          printf("\n4. Exit\n");
+          printf("\nEnter your choice: ");
+          scanf("%d", &choice);     
+
+
+          switch(choice){
+               case 1:
+                   checkBalance(balance);
+                   break;
+               case 2:
+                   balance += deposit();
+                   break;
+               case 3:
+                    balance -= withDraw(balance);
+                    break;
+               case 4:
+                    printf("\nThank you for using the bank.");
+                    break;
+               default:
+                      printf("\nInvalid choice! Please select (1 - 4): \n");
+          }
      }
-     printf("\nHappy new year\n");
+     while(choice != 4);
 
      return 0;
+}
+
+void checkBalance(float balance){
+     printf("\nYour current balance is: $%.2f\n", balance);
+}
+float deposit(){
+     float amount = 0.0f;
+     printf("\nEnter amount to deposit: ");
+     scanf("%f", &amount);
+
+     if(amount < 0){
+          printf("Invalid amount\n");
+          return 0.0f;
+     }
+     else{
+          printf("Successfully deposited $%.2f", amount);
+          return amount;
+     }
+}
+float withDraw(float balance){
+     float amount = 0.0f;
+     printf("\nEnter amount to withdraw: $");
+     scanf("%f", amount);
+     
+     if(amount < 0){
+          printf("Invalid amount\n");
+          return 0.0f;
+     }
+     else if(amount > balance){
+          printf("Insufficient funds! Your baalance is %.2f\n", balance);
+          return 0.0f;
+     }
+     else{
+          printf("Successfully withdrew $%.2f\n", amount);
+          return amount;
+     }
 }
